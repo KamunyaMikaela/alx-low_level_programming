f7e649c21a34c48b058cf6bcc3bd58bc34558390
KamunyaMikaela <kamunyammikaela@gmail.com>
2023-06-19 01:54:43 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main-int main(entry point)
+*return-int main always 0(success)
+*return-always 0(success)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
